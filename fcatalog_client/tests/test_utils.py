7348410f62c745b90796b68d9e5c5b52fd2d1e95
xorpd <xorpd@xorpd.net>
2015-09-22 13:39:37 +0200

    Added blockify for iterators.

---
+++ fcatalog_client/tests/test_utils.py
@@ -0,0 +1,37 @@
+import unittest
+
+from fcatalog_client.utils import blockify
+
+
+class TestBlockify(unittest.TestCase):
+    def test_blockify_7_3(self):
+        """
+        Make blocks of size 3 from a range of size 7.
+        """
+        res = []
+        for b in blockify(range(7),3):
+            res.append(b)
+
+        assert res == [[0,1,2],[3,4,5],[6]]
+
+    def test_blockify_9_3(self):
+        """
+        Test basic operation of blockify.
+        """
+        res = []
+        for b in blockify(range(9),3):
+            res.append(b)
+
+        assert res == [[0,1,2],[3,4,5],[6,7,8]]
+
+    def test_blockify_empty(self):
+        """
+        Try to blockify an empty iterator:
+        """
+        res = []
+        for b in blockify(range(0),5):
+            res.append(b)
+
+        assert len(res) == 0
+
+
